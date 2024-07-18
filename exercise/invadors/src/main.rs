44d037d525db1d179f799648bbb87beec41e5cf4
Gal Brandwine <gbrandwine@augury.com>
2024-07-18 23:43:35 +0300

    Almost Done with the CRASH Course

---
+++ exercise/invadors/src/main.rs
@@ -0,0 +1,117 @@
+use core::fmt;
+use crossterm::{
+    cursor::{Hide, Show},
+    event::{self, Event, KeyCode},
+    terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
+};
+use invadors::{
+    frame::{self, new_frame, Drawable},
+    invaders::Invaders,
+    player, render,
+};
+use rusty_audio::Audio;
+use std::{
+    error::Error,
+    io, thread,
+    time::{Duration, Instant},
+};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut audio = Audio::new();
+    audio.add("explode", "audio/explode.wav");
+    audio.add("lose", "audio/lose.wav");
+    audio.add("move", "audio/move.wav");
+    audio.add("pew", "audio/pew.wav");
+    audio.add("startup", "audio/startup.wav");
+    audio.add("win", "audio/win.wav");
+
+    audio.play("startup");
+
+    // Terminal
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+    // Lets render our frame in a separate thread
+    let (render_tx, render_rx) = crossbeam::channel::unbounded();
+    let frame_handler = std::thread::spawn(move || {
+        let mut last_frame = frame::new_frame();
+        let mut stdout = io::stdout();
+        render::render(&mut stdout, &last_frame, &last_frame, true);
+        loop {
+            let curr_frame = match render_rx.recv() {
+                Ok(x) => x,
+                Err(err) => {
+                    print!("Error! {}", err);
+                    break;
+                } // Will break if the main thread drops the sender
+            };
+            render::render(&mut stdout, &last_frame, &curr_frame, false);
+            last_frame = curr_frame;
+        }
+    });
+
+    // Game loop
+    let mut player = player::Player::new();
+    let mut instant = Instant::now();
+    let mut invaders = Invaders::new();
+    'game_loop: loop {
+        // Per-frame init
+        let delta = instant.elapsed();
+        instant = Instant::now();
+        let mut curr_frame = new_frame();
+
+        // Input
+        while event::poll(Duration::default())? {
+            if let Event::Key(key_event) = event::read()? {
+                match key_event.code {
+                    KeyCode::Left => {
+                        player.move_left();
+                        audio.play("move");
+                    }
+                    KeyCode::Right => {
+                        player.move_right();
+                        audio.play("move");
+                    }
+                    KeyCode::Char(' ') | KeyCode::Enter => {
+                        if player.shoot() {
+                            audio.play("pew");
+                        }
+                    }
+                    KeyCode::Esc | KeyCode::Char('q') => {
+                        audio.play("lose");
+                        print!("You lose!\n");
+                        break 'game_loop;
+                    }
+                    _ => {}
+                }
+            }
+        }
+
+        // Updates
+        player.update(delta);
+        if invaders.update(delta) {
+            audio.play("move");
+        }
+
+        // Draw & render
+        let drawables: Vec<&dyn Drawable> = vec![&player, &invaders];
+        for drawable in drawables {
+            drawable.draw(&mut curr_frame);
+        }
+        let _ = render_tx.send(curr_frame); // We expect this will fail in the first few times - since this will run before the render thread is ready
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    // Cleanup
+    drop(render_tx); // This will cause the render thread to exit.
+    frame_handler.join().unwrap();
+    audio.wait();
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
+
+    Ok(())
+}
---
+++ exercise/invadors/src/render.rs
@@ -0,0 +1,25 @@
+use crate::frame::Frame;
+use crossterm::cursor::MoveTo;
+use crossterm::style::Color;
+use crossterm::style::SetBackgroundColor;
+use crossterm::terminal::{Clear, ClearType};
+use crossterm::QueueableCommand;
+use std::io::Stdout;
+use std::io::Write;
+
+pub fn render(stdout: &mut Stdout, last_frame: &Frame, cur_frame: &Frame, force: bool) {
+    if force {
+        stdout.queue(SetBackgroundColor(Color::Blue)).unwrap();
+        stdout.queue(Clear(ClearType::All)).unwrap();
+        stdout.queue(SetBackgroundColor(Color::Black)).unwrap();
+    }
+    for (x, col) in cur_frame.iter().enumerate() {
+        for (y, s) in col.iter().enumerate() {
+            if *s != last_frame[x][y] || force {
+                stdout.queue(MoveTo(x as u16, y as u16)).unwrap();
+                print!("{}", *s);
+            }
+        }
+    }
+    stdout.flush().unwrap();
+}
